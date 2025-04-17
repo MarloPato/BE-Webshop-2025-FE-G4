d6e1f0d780906eedf1c7ada23978c6e99ca623a5
Gabriel Richter Reynoso <garey91@gmail.com>
2025-04-17 11:41:02 +0200

    add receive category

--- src/classes/product.js
+++ src/classes/product.js
@@ -1,10 +1,10 @@
 export class Product {
-    constructor (name, price, description, stock, imageUrl, category) {
+    constructor (name, price, description, stock, category, imageUrl) {
         this.name = name;
         this.price = price;
         this.description = description;
         this.stock = stock;
-        this.imageUrl = imageUrl;
         this.category = category;
+        this.imageUrl = imageUrl;
     }
 }
